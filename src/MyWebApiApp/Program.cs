0d9a7c649d81e0c5b44705b1e7f88d1a55d177c6
george nairooze <gnairooze@gmail.com>
2025-01-07 19:37:40 +0200

    change example business and add requests to test

--- src/MyWebApiApp/Program.cs
+++ src/MyWebApiApp/Program.cs
@@ -1,44 +1,51 @@
+using SimpleWebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var products = new List<Product>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Product { Id = 1, Name = "Product 1", Price = 10.0m },
+    new Product { Id = 2, Name = "Product 2", Price = 20.0m },
+    new Product { Id = 3, Name = "Product 3", Price = 30.0m }
 };
 
-app.MapGet("/weatherforecast", () =>
+var orders = new List<Order>()
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    new Order { Id = 1, ProductId = 1, Quantity = 1, TotalPrice = 10.0m },
+    new Order { Id = 2, ProductId = 2, Quantity = 2, TotalPrice = 40.0m },
+    new Order { Id = 3, ProductId = 3, Quantity = 3, TotalPrice = 90.0m }
+};
+
+app.MapGet("/api/products", () => products);
+
+app.MapGet("/api/products/{id}", (int id) =>
+{
+    var product = products.FirstOrDefault(p => p.Id == id);
+    return product is not null ? Results.Ok(product) : Results.NotFound();
+});
+
+app.MapPost("/api/products", (Product product) =>
+{
+    product.Id = products.Count + 1;
+    products.Add(product);
+    return Results.Created($"/api/products/{product.Id}", product);
+});
+
+app.MapGet("/api/orders", () => orders);
+
+app.MapGet("/api/orders/{id}", (int id) =>
+{
+    var order = orders.FirstOrDefault(o => o.Id == id);
+    return order is not null ? Results.Ok(order) : Results.NotFound();
+});
+
+app.MapPost("/api/orders", (Order order) =>
+{
+    order.Id = orders.Count + 1;
+    order.TotalPrice = order.Quantity * products.First(p => p.Id == order.ProductId).Price;
+    orders.Add(order);
+    return Results.Created($"/api/orders/{order.Id}", order);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
